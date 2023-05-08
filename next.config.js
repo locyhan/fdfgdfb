0441b8c8cb5436ffa4747b57fb6fcada3e39bb7c
locyhan <nguyenbaloc2822@gmail.com>
2023-05-08 11:45:09 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ module.exports = {
 			process.env.WORDPRESS_API_URL.match(/(http(?:s)?:\/\/)(.*)/)[2], // Valid WP Image domain.
 			'0.gravatar.com',
 			'secure.gravatar.com',
-			'nextwp.local',
+			'onlinepaati.com',
 		],
 	},
 };
